7d8d5fc4763a41389f0d7e614bbf8e962a3993c0
Emmanuel Arias Soto <ariassotoemmanuel@gmail.com>
2023-01-04 20:22:01 +0100

    Create the most basic window

---
+++ rust/tamarindo_engine/src/lib.rs
@@ -0,0 +1,36 @@
+// Copyright 2023 Emmanuel Arias Soto <ariassotoemmanuel@gmail.com>. All rights
+// reserved. Use of this source code is governed by the Apache-2.0 license that
+// can be found in the LICENSE file.
+
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+pub fn run() {
+    env_logger::init();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => {}
+        },
+        _ => {}
+    });
+}
